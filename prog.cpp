271021f3ed647a959628f081ba4866bda52ec5a9
lcc-lgtm <lcc0i8ah1w54o68@gmail.com>
2025-07-28 23:08:44 +0800

    Update prog.cpp

--- prog.cpp
+++ prog.cpp
@@ -11,62 +11,240 @@ int main()
 	cout << setw(33) << "(MID VALLEY SHOPPING MALL)" << endl;
 	cout << setw(25) << "-INCOICE-" << endl;
 	cout << "+                                         +\n";
-	cout << setfill('*') << setw(30) << endl;
+	cout << setfill('*') << setw(30) << "" << endl;
 	cout << "---------Welcome to CSG Gadget Shop--------\n";
 	cout << setfill('*') << setw(30) << endl;
 	cout << setw(8) << "-Welcome to CSG Menu-" << setw(8) << endl;
 
-	string A1 = "A ORDER ENTRY";
-	string B1 = "B REPORTING";
-	string C1 = "C EXIT";
-	string P1,P2,C1,C2;
-	char A, a, B, b, C, c, P, C, U, H, I, S, N, D, O;
+	char A ;
+	char B ;
+	char C ;
+	string P1,P2,T1,T2;
+	cout << left << "A ORDER ENTRY" << endl << left << "B REPORTING" << endl << left << "C EXIT" << endl;
+	cout << "Enter your selection :";
 
-	cout << left << A1 << endl << left << B1 << endl << left << C1 << endl << left << D1 << endl;
-	if (A1 == "A ORDER ENTRY")
+
+	char fst_option;
+	cin >> fst_option;
+	if (fst_option == 'A' || fst_option == 'a')
 	{
+		char secOpt;
 		cout << "Mobile Accessories press a\n" << "Computer Accessories press b\n" << "Enter your selection :";
-		if (a == 'A' || a == 'a')
+		cin >> secOpt;
+
+		if (secOpt == 'a' || secOpt == 'A')
 		{
-			cout << "Category of sales :\n" << "Power & Charging press 'P'\n" << "Cables & Connectivity press 'C'\n"
-				<< "Audio press 'A'\n" << "Protection & Holders press 'H'";
-			if (P == 'P' || P == 'p')
-				cout << "List of sales :" << "Phone Charger press 'P1'\n" << "Power Bank press P2\n";
-			else if (C == 'c' || C == 'C')
-				cout << "List of sales :" << "Type-C USB Cable press 'C1'\n" << "Lightning Cable (for iPhone) press C2\n";
-			else if (U == 'U' || U == 'u')
-				cout << "List of sales :" << "TWS Earbuds press 'U1'\n" << "Bluetooth Speaker press U2\n";
-			else if (H == 'H' || H == 'h')
-				cout << "List of sales :" << "Phone Case press 'H1'\n" << "Screen Protector press H2\n";
+			char thdOpt;
+			cout << "Category of sales :\n" 
+				<< "Power & Charging press      'P'\n" 
+				<< "Cables & Connectivity press 'T'\n"
+				<< "Audio press                 'A'\n" 
+				<< "Protection & Holders press  'H'\n"
+				<< "Enter your option :";
+			cin >> thdOpt;
+			if (thdOpt == 'P' || thdOpt == 'p')
+			{
+				char fthOpt;
+				cout << "List of sales :" << "Phone Charger press    'P1'\n" << "Power Bank press        P2\n"
+					<< "Enter your option :";
+				cin >> fthOpt;
+				if (fthOpt == 'P1')
+				{
+					double prz, total = 0;
+					int qtyPro;
+					cout << "Quantity of product";
+					cin >> qtyPro;
+				}
+				else if (fthOpt == 'P2')
+				{
+					double prz, total = 0;
+					int qtyPro;
+					cout << "Quantity of product";
+					cin >> qtyPro;
+				}
+				else
+			}
+			else if (thdOpt == 't' || thdOpt == 'T')
+			{
+				char fthOpt;
+				cout << "List of sales :" << "Type-C USB Cable press 'T1'\n" << "Lightning Cable press   T2\n"
+					<< "Enter your option :";
+				cin >> fthOpt;
+				if (fthOpt == 'P1')
+				{
+					double prz, total = 0;
+					int qtyPro;
+					cout << "Quantity of product";
+					cin >> qtyPro;
+				}
+				else if (fthOpt == 'P2')
+				{
+					double prz, total = 0;
+					int qtyPro;
+					cout << "Quantity of product";
+					cin >> qtyPro;
+				}
+				else
+			}
+			else if (thdOpt == 'U' || thdOpt == 'u')
+			{
+				char fthOpt;
+				cout << "List of sales :" << "TWS Earbuds press      'U1'\n" << "Bluetooth Speaker press U2\n"
+					<< "Enter your option :";
+				cin >> fthOpt;
+				if (fthOpt == 'P1')
+				{
+					double prz, total = 0;
+					int qtyPro;
+					cout << "Quantity of product";
+					cin >> qtyPro;
+				}
+				else if (fthOpt == 'P2')
+				{
+					double prz, total = 0;
+					int qtyPro;
+					cout << "Quantity of product";
+					cin >> qtyPro;
+				}
+				else
+			}
+			else if (thdOpt == 'H' || thdOpt == 'h')
+			{
+				char fthOpt;
+				cout << "List of sales :" << "Phone Case press       'H1'\n" << "Screen Protector press  H2\n"
+					<< "Enter your option :";
+				cin >> fthOpt;
+				if (fthOpt == 'P1')
+				{
+					double prz, total = 0;
+					int qtyPro;
+					cout << "Quantity of product";
+					cin >> qtyPro;
+				}
+				else if (fthOpt == 'P2')
+				{
+					double prz, total = 0;
+					int qtyPro;
+					cout << "Quantity of product";
+					cin >> qtyPro;
+				}
+				else
+			}
 			else
 				cout << "Invalid code !!!\n" << "Please return to homepage." << endl;
 		}
-		else if (b == 'B' || b == 'b')
+		else if (secOpt == 'B' || secOpt == 'b')
 		{
-			cout << "Category of sales :\n" << "Input & Audio press 'I'\n" << "Storage press 'S'\n"
-				<< "Connectivity & Power press 'N'\n" << "Visual & Display press 'D'" << "Office & Productivity 'O'" << endl;
-			if (I == 'I' || I == 'i')
-				cout << "List of sales :" << "Wireless Mouse press 'I1'\n" << "Headset with Microphone press I2\n";
-			else if (S == 'S' || S == 's')
-				cout << "List of sales :" << "USB Flash Drive press 'S1'\n" << "External Hard Drive (Hard Disc) press S2\n";
-			else if (N == 'N' || N == 'n')
-				cout << "List of sales :" << "Laptop Charger press 'N1'\n" << "USB Hub press N2\n";
-			else if (D == 'D' || D == 'd')
-				cout << "List of sales :" << "Monitor screen press 'D1'\n" << "HDMI Cable press D2\n";
-			else if (O == 'o' || O == 'O')
-				cout << "List of sales :" << "Printer press 'O1'\n" << "Stylus Pen (for iPad or touchscreen laptop) press O2\n";
+			char thdOpt;
+			cout << "Category of sales :\n" 
+				<< "Input & Audio press        'I'\n" 
+				<< "Storage press              'S'\n"
+				<< "Connectivity & Power press 'N'\n" 
+				<< "Visual & Display press     'D'\n" 
+				<< "Office & Productivity      'O'\n" 
+				<< "Enter your option :" ;
+			cin >> thdOpt;
+			if (thdOpt == 'I' || thdOpt == 'i')
+			{
+				char fthOpt;
+				cout << "List of sales :" << "Wireless Mouse press  'I1'\n" << "Headset with Microphone press I2\n"
+					<< "Enter your option :";
+				cin >> fthOpt;
+				if (fthOpt == 'P1')
+				{
+					double prz, total = 0;
+					int qtyPro;
+					cout << "Quantity of product";
+					cin >> qtyPro;
+				}
+				else if (fthOpt == 'P2')
+				{
+					double prz, total = 0;
+					int qtyPro;
+					cout << "Quantity of product";
+					cin >> qtyPro;
+				}
+				else
+			}
+			else if (thdOpt == 'S' || thdOpt == 's')
+			{
+				char fthOpt;
+				cout << "List of sales :" << "USB Flash Drive press 'S1'\n" << "External Hard Drive press     S2\n"
+					<< "Enter your option :";
+				cin >> fthOpt;
+				if (fthOpt == 'P1')
+				{
+					double prz, total = 0;
+					int qtyPro;
+					cout << "Quantity of product";
+					cin >> qtyPro;
+				}
+				else if (fthOpt == 'P2')
+				{
+					double prz, total = 0;
+					int qtyPro;
+					cout << "Quantity of product";
+					cin >> qtyPro;
+				}
+				else
+			}
+			else if (thdOpt == 'N' || thdOpt == 'n')
+			{
+				char fthOpt;
+				cout << "List of sales :" << "Laptop Charger press  'N1'\n" << "USB Hub press                 N2\n"
+					<< "Enter your option :";
+				cin >> fthOpt;
+				if (fthOpt == 'P1')
+				{
+					double prz, total = 0;
+					int qtyPro;
+					cout << "Quantity of product";
+					cin >> qtyPro;
+				}
+				else if (fthOpt == 'P2')
+				{
+					double prz, total = 0;
+					int qtyPro;
+					cout << "Quantity of product";
+					cin >> qtyPro;
+				}
+				else
+			}
+			else if (thdOpt == 'D' || thdOpt == 'd')
+			{
+				char fthOpt;
+				cout << "List of sales :" << "Monitor screen press  'D1'\n" << "HDMI Cable press              D2\n"
+					<< "Enter your option :";
+				cin >> fthOpt;
+				if (fthOpt == 'P1')
+				{
+					double prz, total = 0;
+					int qtyPro;
+					cout << "Quantity of product";
+					cin >> qtyPro;
+				}
+				else if (fthOpt == 'P2')
+				{
+					double prz, total = 0;
+					int qtyPro;
+					cout << "Quantity of product";
+					cin >> qtyPro;
+				}
+				else
+			}
+			else if (thdOpt == 'o' || thdOpt == 'O')
+				cout << "List of sales :" << "Printer press         'O1'\n" << "Stylus Pen(touchscreen) press O2\n";
 			else
 				cout << "Invalid code !!!\n" << "Please return to homepage." << endl;
 		}
 		else
 			cout << "Invalid code !!!\n" << "Please return to homepage." << endl;
 	}
-	else if (B1 == "B REPORTING") // report于此地
+	else if (fst_option == 'B' || fst_option == 'b') // report于此地
 	{
 		cout << "咋办" << endl;
-
 	}
-	else if (C1 == "C EXIT")
+	else if (fst_option == 'C' || fst_option == 'c')
 		cout << "Thank you for using this system.\n" << "See your next time!!!" << endl;
 
 	return 0;
