0d5c2b2ec8c7fcf0f32b60d4c4f1b60f54eac51b
lcc-lgtm <lcc0i8ah1w54o68@gmail.com>
2025-07-28 11:36:16 +0800

    Update prog.cpp

--- prog.cpp
+++ prog.cpp
@@ -7,5 +7,35 @@ int main()
 dsdsda
 
 abcd
+ #include<iostream>
+#include<iomanip>
+using namespace std;
 
-] 
+int main()
+{
+	cout << setw(29) << "CSG Gadget Shop"<< endl;
+	cout << setw(33) << "LOT 1851-A, Jalan KPB 6," << endl;
+	cout << setw(36) << "KAWASAN PERINDUSTRIAN BELAKONG" << endl;
+	cout << setw(36) << "43300 SERI KEMBANGAN, SELANGOR" << endl;
+	cout << setw(33) << "(MID VALLEY SHOPPING MALL)" << endl;
+	cout << setw(25) << "-INCOICE-" << endl;
+	cout << "+                                         +\n";
+	cout << setfill('*') << setw(30) << endl;
+	cout << "---------Welcome to CSG Gadget Shop--------\n";
+	cout << setfill('*') << setw(30) << endl;
+	cout << setw(8) << "-Welcome to CSG Menu-" << setw(8) << endl;
+
+	cout << left << "A ORDER ENTRY" << endl << left << "B REPORTING" << endl << left << "C EXIT" << endl;
+
+	string leftText = "A - PHONE";
+	string rightText = "B - EARPHONE"; 
+
+	cout << leftText << "A - PHONE" << rightText << "B - EARPHONE" << endl;
+
+
+
+	return 0; 
+}
+
+
+]
